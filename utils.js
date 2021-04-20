79e038e3f8af7bd9b213e82555040a03b5e1584f
Andre Staltz <andre@staltz.com>
2021-04-20 15:19:55 +0300

    move code into dedicated files

---
+++ utils.js
@@ -0,0 +1,14 @@
+function flat(err) {
+  if(!err) return err
+  if(err === true) return true
+  return {message: err.message, name: err.name, stack: err.stack}
+}
+
+function closedread (msg) {
+  console.error('packet-stream asked to read after closed', msg)
+}
+
+module.exports = {
+  flat: flat,
+  closedread: closedread
+}
